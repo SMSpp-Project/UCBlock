7a2384879f1248742305bb0580c1bc7c7b64eb98
Antonio Frangioni <frangio@di.unipi.it>
2019-07-19 10:56:09 +0200

    Update BusNetworkBlock.h

--- include/BusNetworkBlock.h
+++ include/BusNetworkBlock.h
@@ -4,7 +4,8 @@
 /** @file
  * Header file for the class BusNetworkBlock, which derives from NetworkBlock
  * [see NetworkBlock.h] in order to define a "bus" transmission network in
- * the Unit Commitment problem.
+ * the Unit Commitment problem. There is actually precious little that this
+ * class has to do that is not done already by the base NetworkBlock class.
  *
  * \version 0.11
  *
@@ -39,7 +40,7 @@
 
 #ifndef __BusNetworkBlock
 #define __BusNetworkBlock
-/* self-identification: #endif at the end of the file */
+                      /* self-identification: #endif at the end of the file */
 
 /*--------------------------------------------------------------------------*/
 /*------------------------------ INCLUDES ----------------------------------*/
@@ -60,9 +61,11 @@ namespace SMSpp_di_unipi_it {
 /*--------------------------------------------------------------------------*/
 /// A NetworkBlock with only one node, i.e., a "bus" transmission network
 /** The BusNetworkBlock class, which derives from NetworkBlock [see
- *  NetworkBlock.h] implements the Block concept [see Block.h] in order to
- *  define a "bus" transmission network in the Unit Commitment problem for a
- *  given instant in the time horizon. */
+ * NetworkBlock.h] implements the Block concept [see Block.h] in order to
+ * define a "bus" transmission network in the Unit Commitment problem for a
+ * given instant in the time horizon. There is actually precious little that
+ * this class has to do that is not done already by the base NetworkBlock
+ * class.*/
 
 class BusNetworkBlock : public NetworkBlock {
 
@@ -78,13 +81,13 @@ class BusNetworkBlock : public NetworkBlock {
 /** @name Constructor and Destructor
  *  @{ */
 
- /// Constructor of BusNetworkBlock, taking possibly a pointer to its father
+ /// constructor of BusNetworkBlock, taking possibly a pointer to its father
 
  explicit BusNetworkBlock( Block * f_block = nullptr ) :
   NetworkBlock( f_block ) {}
 
 /*--------------------------------------------------------------------------*/
- /// Destructor of BusNetworkBlock, (understandably) does nothing
+ /// destructor of BusNetworkBlock, (understandably) does nothing
 
  ~BusNetworkBlock() override = default;
 
@@ -94,17 +97,17 @@ class BusNetworkBlock : public NetworkBlock {
 /** @name Other initializations
  *  @{ */
 
-/// Loads the BusNetworkBlock instance from memory
+/// loads the BusNetworkBlock instance from memory
 /** Like load( std::istream & ), if there is any Solver attached to this
  *  BusNetworkBlock then a NBModification (the "nuclear option") is issued.
  */
 
  void load( std::istream & input ) override {
   throw ( std::logic_error( "BusNetworkBlock::load() not implemented yet" ) );
- }
+  }
 
 /*--------------------------------------------------------------------------*/
-/// Generates the static variables of BusNetworkBlock
+/// generates the static variables of BusNetworkBlock
 /** The base BusNetworkBlock class has just the node injection variables.
  * Since a "bus" network has just one node, and therefore a single value D for
  * the demand and a single injection variable s, which can hardly be called a
@@ -112,18 +115,6 @@ class BusNetworkBlock : public NetworkBlock {
  * having s = D which in fact makes the variable a constant.*/
 
  void generate_abstract_variables( Configuration * stvv ) override;
-
-/*--------------------------------------------------------------------------*/
-/*--------------- METHODS FOR MODIFYING THE BusNetworkBlock ----------------*/
-/*--------------------------------------------------------------------------*/
-/** @name Methods for modifying the BusNetworkBlock
- *  @{ */
-
- /// Method to set the NetworkData object
- /** This method does nothing in BusNetworkBlock because by definition
-  * the network is made by only one node. */
-
- void set_NetworkData( NetworkBlock::NetworkData * nd ) override {}
 
 /**@} ----------------------------------------------------------------------*/
 /*-------------------- PROTECTED PART OF THE CLASS -------------------------*/
