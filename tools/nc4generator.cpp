a001a24cf6ea4fd4d2fbe8c089f3d184b03c4366
Niccolò Iardella <niccolo.iardella@for.unipi.it>
2019-12-09 11:44:18 +0100

    Fix MaxFlow values

--- tools/nc4generator.cpp
+++ tools/nc4generator.cpp
@@ -513,7 +513,7 @@ void serialize_thermalunit( netCDF::NcGroup & g, const ThermalUnit & unit ) {
 
 void serialize_hydrounit( netCDF::NcGroup & g, const HydroUnit & unit ) {
  serialize( g, "LinearTerm", netCDF::NcDouble(), unit.volumeToPower );
- serialize( g, "MaxFlow", netCDF::NcDouble(), unit.maxUsage );
+ serialize( g, "MaxFlow", netCDF::NcDouble(), unit.maxSpillage );
  serialize( g, "InitialVolumetric", netCDF::NcDouble(), unit.initialFlood );
  serialize( g, "MinVolumetric", netCDF::NcDouble(), unit.minFlood );
  serialize( g, "MaxVolumetric", netCDF::NcDouble(), unit.maxFlood );
