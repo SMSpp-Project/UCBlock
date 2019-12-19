454f657711e28d4beb73fbadb61789f4b9a3f67c
Ali Ghezel <ali.nov7@gmail.com>
2019-12-19 12:00:10 +0100

    New UnitBlock interface update

--- include/BatteryUnitBlock.h
+++ include/BatteryUnitBlock.h
@@ -834,17 +834,19 @@ class BatteryUnitBlock : public UnitBlock {
   return v_battery_binary;
  }
 
-
+/*--------------------------------------------------------------------------*/
+ /// returns the vector of active_power variables
  ColVariable * get_active_power( Index generator ) override {
   return &( v_active_power.front() );
  }
 /*--------------------------------------------------------------------------*/
-
+ /// returns the vector of primary_spinning_reserve variables
  ColVariable * get_primary_spinning_reserve( Index generator ) override {
   return &( v_primary_spinning_reserve.front() );
  }
 
 /*--------------------------------------------------------------------------*/
+ /// returns the vector of secondary_spinning_reserve variables
  ColVariable * get_secondary_spinning_reserve( Index generator ) override {
   return &( v_secondary_spinning_reserve.front() );
  }
--- include/ThermalUnitBlock.h
+++ include/ThermalUnitBlock.h
@@ -936,22 +936,23 @@ class ThermalUnitBlock : public UnitBlock {
  * - shut_down variables;
  *
  * @{ */
-
+ /// returns the vector of commitment variables
  ColVariable * get_commitment( Index generator ) override {
   return &( v_commitment.front() );
  }
 /*--------------------------------------------------------------------------*/
-
+ /// returns the vector of active_power variables
  ColVariable * get_active_power( Index generator ) override {
   return &( v_active_power.front() );
  }
 /*--------------------------------------------------------------------------*/
-
+ /// returns the vector of primary_spinning_reserve variables
  ColVariable * get_primary_spinning_reserve( Index generator ) override {
   return &( v_primary_spinning_reserve.front() );
  }
 
 /*--------------------------------------------------------------------------*/
+ /// returns the vector of secondary_spinning_reserve variables
  ColVariable * get_secondary_spinning_reserve( Index generator ) override {
   return &( v_secondary_spinning_reserve.front() );
  }
