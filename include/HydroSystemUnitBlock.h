b0db0bb5f0fb6bf9b6f5221c22115aebeafb3109
Ali Ghezel <ali.nov7@gmail.com>
2020-01-21 17:49:59 +0100

    Register PolyhedralFunctionBlock

--- include/HydroSystemUnitBlock.h
+++ include/HydroSystemUnitBlock.h
@@ -45,6 +45,8 @@
 
 namespace SMSpp_di_unipi_it {
 
+class HydroUnitBlock;     // forward declaration of HydroUnitBlock
+
 /*--------------------------------------------------------------------------*/
 /*----------------------- CLASS HydroSystemUnitBlock -----------------------*/
 /*--------------------------------------------------------------------------*/
@@ -114,12 +116,44 @@ class HydroSystemUnitBlock : public Block {
  *   (HydroUnitBlock) in the problem;
  *
  * - The groups "HydroUnitBlock_0", "HydroUnitBlock_1", ... ,
- *   "HydroUnitBlock_n" with n == NumberUnits - 1, containing each one
- *   HydroUnitBlock corresponding to one or more electricity generating unit
- *   (electrical generator). It is an error if the corresponding groups are
- *   not there. Each HydroUnitBlock can have more than one electrical
- *   generator.
+ *   "HydroUnitBlock_( n - 1 )", "PolyhedralFunctionBlock_n" with
+ *   n == NumberHydroUnits, containing each one HydroUnitBlock, and the last
+ *   one is corresponding to the polyhedral function block of the UCBlock.
  *
+ * - The groups "BellmanValue_0", "BellmanValue_1", ... ,"BellmanValue_R" with
+ *   R == TotalNumberReservoirs, containing the future value of water
+ *   (Volumetric variable) of each reservoir in each HydroUnitBlock. Since
+ *   each HydroUnitBlock can have more than one reservoir(cf. HydroUnitBlock::
+ *   get_number_reservoirs()), a value that is useful in the following is the
+ *   total number of reservoirs. We will refer to such number as
+ *   "TotalNumberReservoirs", which is computed by just calling
+ *   get_number_reservoirs() on each of the HydroUnitBlock and summing all the
+ *   results. Clearly, TotalNumberReservoirs >= NumberHydroUnits. Some of the
+ *   HydroUnitBlock may have just one reservoir; if this happens for all the
+ *   hydro unit blocks (but this is not likely), then TotalNumberReservoirs ==
+ *   NumberHydroUnits. It is then useful to be able to assign a unique index
+ *   h = 0, 1, ..., NumberHydroUnits - 1 to each of the hydro unit block in
+ *   the UCBlock. When TotalNumberReservoirs == NumberHydroUnits the index is
+ *   the same as b = 0, 1, ..., NumberHydroUnits - 1 (there is a one-to-one
+ *   correspondence between HydroUnitBlock and reservoir, but this is not
+ *   likely to happen). When, instead, TotalNumberReservoirs >
+ *   NumberHydroUnits, a mapping must be defined. The mapping is the obvious
+ *   one: HydroUnitBlock have an ordering b = 0, 1, ..., NumberHydroUnits - 1
+ *   (cf. the groups "HydroUnitBlock_0", "HydroUnitBlock_1", ... above), and
+ *   the number of reservoirs into each HydroUnitBlock also have some natural
+ *   ordering, Thus, in general the mapping is:
+ *
+ *     bellman value 0 = first reservoir of HydroUnitBlock_0
+ *     bellman value 1 = second reservoir of HydroUnitBlock_0
+ *     ...
+ *     bellman value k = k-th reservoir of HydroUnitBlock_0
+ *                    k = HydroUnitBlock_0->get_number_reservoirs()
+ *     bellman value k + 1 = first reservoir of HydroUnitBlock_1
+ *     bellman value k + 2 = second reservoir of HydroUnitBlock_1
+ *     ...
+ *
+ *   which of course boils down to "h = b" when each HydroUnitBlock has
+ *   exactly one reservoir (but this is not assumed to happen).
  */
  void deserialize( netCDF::NcGroup & group ) override;
 
@@ -143,18 +177,18 @@ class HydroSystemUnitBlock : public Block {
 /** @name Methods for loading, printing & saving the HydroSystemUnitBlock
  *  @{ */
 
- /// extends Block::serialize( netCDF::NcGroup )
- /** Extends Block::serialize( netCDF::NcGroup ) to the specific format of a
-  *  HydroSystemUnitBlock. See HydroSystemUnitBlock::deserialize( netCDF::NcGroup ) for
-  *  details of the format of the created netCDF group. */
+/// extends Block::serialize( netCDF::NcGroup )
+/** Extends Block::serialize( netCDF::NcGroup ) to the specific format of a
+ *  HydroSystemUnitBlock. See HydroSystemUnitBlock::deserialize( netCDF::NcGroup ) for
+ *  details of the format of the created netCDF group. */
 
  void serialize( netCDF::NcGroup & group ) const override;
 
 /**@} ----------------------------------------------------------------------*/
 /*------------- METHODS FOR MODIFYING THE HydroSystemUnitBlock -------------*/
 /*--------------------------------------------------------------------------*/
- /** @name Methods for modifying the HydroSystemUnitBlock
-  *  @{ */
+/** @name Methods for modifying the HydroSystemUnitBlock
+ *  @{ */
 
 /**@} ----------------------------------------------------------------------*/
 /*------------ METHODS FOR INITIALIZING THE HydroSystemUnitBlock -----------*/
@@ -182,6 +216,15 @@ class HydroSystemUnitBlock : public Block {
 /*-------------------- PROTECTED FIELDS OF THE CLASS -----------------------*/
 /*--------------------------------------------------------------------------*/
 
+/*--------------------------------data--------------------------------------*/
+ /// The number of hydro units of the problem
+ Index f_number_hydro_units;
+
+ /// The set of HydroUnitBlock
+ std::vector< HydroUnitBlock * > v_hydro_unit_blocks;
+
+ /// The set of BellmanValues
+ std::vector< HydroUnitBlock * > v_bellman_values;
 /*--------------------------------------------------------------------------*/
 /*--------------------- PRIVATE PART OF THE CLASS --------------------------*/
 /*--------------------------------------------------------------------------*/
