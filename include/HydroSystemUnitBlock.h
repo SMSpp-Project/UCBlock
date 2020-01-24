100327901ee90235edbd65f58ab348d417c0e2a2
Antonio Frangioni <frangio@di.unipi.it>
2020-01-24 13:59:41 +0100

    Update HydroSystemUnitBlock.h

--- include/HydroSystemUnitBlock.h
+++ include/HydroSystemUnitBlock.h
@@ -113,58 +113,67 @@ class HydroSystemUnitBlock : public Block {
  *   (HydroUnitBlock) in the problem.
  *
  * - The groups "HydroUnitBlock_0", "HydroUnitBlock_1", ... ,
- *   "HydroUnitBlock_(n-1)", "PolyhedralFunctionBlock" with
- *   n == NumberHydroUnits, containing each one HydroUnitBlock, and plus the
- *   last one which is corresponding to the polyhedral function block of the
- *   problem.
+ *   "HydroUnitBlock_(n-1)", with n == NumberHydroUnits, containing each one
+ *   HydroUnitBlock.
  *
- * Note: Bellman values is represented by a single PolyhedralFunction which
- *   lives inside the PolyhedralFunctionBlock. The PolyhedralFunction is
- *   considered as X which is a R_vector of pointers to ColVariable, where
- *   each element of that represents the Volumetric variable of each reservoir
- *   of each HydroUnitBlock at the end of time horizon. Note that the order of
- *   the variables in X is crucial, since that will dictate the order of the
- *   active ColVariable of the PolyhedralFunction. The elements of vector X
- *   are assumed as "X_0", "X_1", ... ,"X_R" where R == TotalNumberReservoirs.
- *   Since each HydroUnitBlock can have more than one reservoir (cf.
- *   HydroUnitBlock:: get_number_reservoirs()) a value that is useful in the
- *   following is the total number of reservoirs. We will refer to such number
- *   as "TotalNumberReservoirs", which is computed by just calling
- *   get_number_reservoirs() on each of the HydroUnitBlock and summing all the
- *   results. Clearly, TotalNumberReservoirs >= NumberHydroUnits. Some of the
- *   HydroUnitBlock may have just one reservoir; if this happens for all the
- *   hydro unit blocks (but this is not likely), then TotalNumberReservoirs ==
- *   NumberHydroUnits. It is then useful to be able to assign a unique index
- *   h = 0, 1, ..., TotalNumberReservoirs - 1 to each of the reservoir in the
- *   problem. When TotalNumberReservoirs == NumberHydroUnits the index is
- *   the same as n = 0, 1, ..., NumberHydroUnits - 1 (there is a one-to-one
- *   correspondence between HydroUnitBlock and reservoir, but this is not
- *   likely to happen). When, instead, TotalNumberReservoirs >
- *   NumberHydroUnits, a mapping must be defined. The mapping is the obvious
- *   one: HydroUnitBlock have an ordering n = 0, 1, ..., NumberHydroUnits - 1
- *   (cf. the groups "HydroUnitBlock_0", "HydroUnitBlock_1", ... above), and
- *   the number of reservoirs into each HydroUnitBlock also have some natural
- *   ordering, Thus, in general the mapping is:
+ * - The group "PolyhedralFunctionBlock" which contains a
+ *   PolyhedralFunctionBlock, whose PolyhedralFunction represents the
+ *   future value of the water (a.k.a. "Bellman values") left at the end of
+ *   the time horizon in all the reservoirs of all the HydroUnitBlock of the
+ *   HydroSystemUnitBlock.
  *
- *   X_0 = volumetric variable of the first reservoir of HydroUnitBlock_0 at
- *                                                          the end of horizon
- *   X_1 = volumetric variable of the second reservoir of HydroUnitBlock_0 at
- *                                                          the end of horizon
+ * The future value of water function is represented by the single
+ * PolyhedralFunction which lives inside the PolyhedralFunctionBlock. The
+ * vector of "active" variable of PolyhedralFunction is therefore in a
+ * one-to-one correspondence with the set of ColVariable in the HydroUnitBlock
+ * that represent the amount of water left in each reservoir at the end of
+ * the time horizon. Thus, it is necessary to specify the order of the
+ * active ColVariable of the PolyhedralFunction. Let us denote by X[ 0 ],
+ * X[ 1 ], ... , X[ R - 1 ] the vector of active ColVariable (i.e.,
+ * X[ i ] is the one returned by get_active_var( i ) and R =
+ * get_num_active_var()). Since each HydroUnitBlock can have more than one
+ * reservoir (cf. HydroUnitBlock::get_number_reservoirs()), R is just the
+ * total number of reservoir, which is computed by just calling
+ * get_number_reservoirs() on each of the HydroUnitBlock and summing all the
+ * results. Clearly, R >= NumberHydroUnits. Some of the HydroUnitBlock may
+ * have just one reservoir; if this happens for all the hydro unit blocks
+ * (but this is not likely), then R == NumberHydroUnits. In this case the
+ * mapping is obvious: X[ i ] is the ColVariable that represent the amount of
+ * water left in the only reservoir of HydroUnitBlock_i at the end of the
+ * time horizon. When, instead, R > NumberHydroUnits, a mapping must be
+ * defined. The mapping is the obvious one: HydroUnitBlock have an ordering
+ * n = 0, 1, ..., NumberHydroUnits - 1  (cf. the groups "HydroUnitBlock_0",
+ * "HydroUnitBlock_1", ... above), and the reservoirs into each
+ * HydroUnitBlock also have a natural ordering, Thus, in general the mapping
+ * is:
+ *
+ *   X[ 0 ] = ColVariable representing the amount of water left in the first
+ *            reservoir of HydroUnitBlock_0 at the end of the time horizon
+ *
+ *   X[ 1 ] = ColVariable representing the amount of water left in the second
+ *            reservoir of HydroUnitBlock_0 at the end of the time horizon
+ *
  *     ...
  *
- *   X_k = volumetric variable of the k_th reservoir of HydroUnitBlock_0 at
- *                                                          the end of horizon
- *                               k = HydroUnitBlock_0->get_number_reservoirs()
+ *   X[ k ] = ColVariable representing the amount of water left in the k-th
+ *            reservoir of HydroUnitBlock_0 at the end of the time horizon,
+ *            with k = HydroUnitBlock_0->get_number_reservoirs()
  *
- *   X_(k + 1) = volumetric variable of the first reservoir of
- *                                      HydroUnitBlock_1 at the end of horizon
- *   X_(k + 2) = volumetric variable of the second reservoir of
- *                                      HydroUnitBlock_1 at the end of horizon
+ *   X[ k + 1 ] = ColVariable representing the amount of water left in the
+ *                first reservoir of HydroUnitBlock_1 at the end of the time
+ *                horizon
+ *
+ *   X[ k + 2 ] = ColVariable representing the amount of water left in the
+ *                second reservoir of HydroUnitBlock_1 at the end of the time
+ *                horizon
  *     ...
  *
- *   which of course boils down to "h = n" when each HydroUnitBlock has
- *   exactly one reservoir (but this is not assumed to happen).
- */
+ * This must be the format of the data (linear inequalities) that define the
+ * PolyhedralFunction: the i-th entry of each vector is related to the
+ * future value of the water stored in the reservoir identified by the
+ * above mapping. See PolyhedralFunction::deserialize() for details about
+ * how the data must be stored in the PolyhedralFunctionBlock group. */
+
  void deserialize( netCDF::NcGroup & group ) override;
 
 /**@} ----------------------------------------------------------------------*/
