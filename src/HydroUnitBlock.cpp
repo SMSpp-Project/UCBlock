d461533a34c7cf8243eb85691b394dac684e4fd2
Niccolò Iardella <niccolo.iardella@for.unipi.it>
2020-06-18 17:28:52 +0200

    Update HydroUnitBlock.cpp

--- src/HydroUnitBlock.cpp
+++ src/HydroUnitBlock.cpp
@@ -663,128 +663,127 @@ void HydroUnitBlock::generate_abstract_constraints( Configuration *stcc ) {
   assert( FinalVolumeReservoir_Const.empty());
   FinalVolumeReservoir_Const.resize
           ( boost::multi_array< FRowConstraint, 2 >::
-            extent_gen()[f_time_horizon][ f_number_reservoirs ] );
+            extent_gen()[1][ f_number_reservoirs ] );
  }
 
  for( Index n = 0; n < f_number_reservoirs; ++n ) {
 
-  std::vector<Index> count( get_number_reservoirs() );
+  auto * lf = new LinearFunction();
 
-  for( Index l = 0 ; l < get_number_generators() ; ++l ) {
+  // Index count = 0;
+  // for( Index l = 0 ; l < get_number_generators() ; ++l ) {
+  //  if( v_uphill_delay[l] == 0 && v_downhill_delay[l] == 0 ) {
+  //   if (v_start_arc[l] == n || v_end_arc[l] == n) {
+  //    count++;
+  //   }
+  //  }
+  // }
 
-   if( v_uphill_delay[l] == 0 &&
-       v_downhill_delay[l] == 0 ) {
+  // initialize the vectors of coefficients, and reset count
+  // LinearFunction::v_coeff_pair coeffs(count + 1);
+  // count = 0;
 
-    count[v_start_arc[l]]++;
-    count[v_end_arc[l]]++;
-
-   }
-  }
-
-  // initialize the vectors of coefficients, and reset count[]
-  std::vector< LinearFunction::v_coeff_pair > coeffs( get_number_generators() );
-
-  for( Index n = 0 ; n < get_number_reservoirs() ; ++n ) {
-   coeffs[ n ].resize( count[ n ] );
-   count[ n ] = 0;
-  }
-
-
-  for( Index l = 0; l < get_number_generators(); ++l ) {
-
-   if( -v_uphill_delay[l] >= 0 &&
-       -v_uphill_delay[l] <= f_time_horizon ) {
-    coeffs[v_start_arc[l]][count[v_start_arc[l]]++] =
-            std::make_pair( &v_flow_rate[0 - v_uphill_delay[l]][l], double( 1 ));
-   }
-   if( v_end_arc[l] == n && v_downhill_delay[l] == 0 ) {
-    coeffs[v_end_arc[l]][count[v_end_arc[l]]++] =
-            std::make_pair( &v_flow_rate[0 - v_downhill_delay[l]][l], double( -1 ));
-   }
-  }
-
-  coeffs[n][0] = std::make_pair( &v_volumetric[n][0], double( 1 ));
-
-  FinalVolumeReservoir_Const[0][n].set_both( v_initial_volumetric[n] + v_inflows[n][0] );
-  FinalVolumeReservoir_Const[0][n].set_function( new LinearFunction( std::move( coeffs[ n ] ) , 0 ) );
- }
- // Index end = 0;
-
- for( Index n = 0; n < f_number_reservoirs; ++n ) {
-
-  auto l_f = new LinearFunction();
-
-  l_f->add_variable( &v_volumetric[n][0], 1.0 );
-
-  // end += n;
+  // for( Index n = 0 ; n < get_number_reservoirs() ; ++n ) {
+  //  coeffs[ n ].resize( count[ n ] );
+  //  count[ n ] = 0;
+  // }
 
   for( Index l = 0; l < f_number_arcs; ++l ) {
 
-   if( v_start_arc[l] == n ) {
-    if( -v_uphill_delay[l] >= 0 &&
-        -v_uphill_delay[l] <= f_time_horizon ) {
-     l_f->add_variable( &v_flow_rate[0 - v_uphill_delay[l]][l], 1.0 );
-    } else if( v_uphill_delay[l] == 0 ) {
-     l_f->add_variable( &v_flow_rate[0][l], 1.0 );
-    }
+   if( -v_uphill_delay[ l ] >= 0 &&
+       -v_uphill_delay[ l ] <= f_time_horizon &&
+       v_start_arc[ l ] == n ) {
+    lf->add_variable( &v_flow_rate[ 0 ][ l ], 1.0 );
    }
 
-   if( v_end_arc[l] == n ) {
-    if( -v_downhill_delay[l] >= 0 &&
-        -v_downhill_delay[l] <= f_time_horizon ) {
-     l_f->add_variable( &v_flow_rate[0 - v_downhill_delay[l]][l], -1.0 );
-    } else if( v_downhill_delay[l] == 0 ) {
-     l_f->add_variable( &v_flow_rate[0][l], -1.0 );
-    }
+   if( v_downhill_delay[ l ] == 0 &&
+       v_end_arc[ l ] == n ) {
+    lf->add_variable( &v_flow_rate[ 0 ][ l ], -1.0 );
    }
   }
 
+  lf->add_variable( &v_volumetric[n][0], 1.0);
+  //
   FinalVolumeReservoir_Const[0][n].set_both( v_initial_volumetric[n] + v_inflows[n][0] );
-  FinalVolumeReservoir_Const[0][n].set_function( l_f );
+  FinalVolumeReservoir_Const[0][n].set_function( lf );
+ }
+ // Index end = 0;
 
-
-
-  for( Index t = 1, constraint_index = 1; t < f_time_horizon;
-       ++t, ++constraint_index  ) {
-
-   auto linear_function = new LinearFunction();
-
-   linear_function->add_variable( &v_volumetric[n][t], 1.0 );
-   linear_function->add_variable( &v_volumetric[n][t-1], -1.0 );
-
-   for( Index l = 0; l < f_number_arcs; ++l ) {
-
-    if( !v_start_arc.empty()) {
-     if( v_start_arc[l] == n ) {
-      if( t - v_uphill_delay[l] >= 0 &&
-          t - v_uphill_delay[l] <= f_time_horizon ) {
-
-       linear_function->add_variable( &v_flow_rate[t - v_uphill_delay[l]][l], 1.0 );
-
-      } else if( v_uphill_delay[l] == 0 ) {
-
-       linear_function->add_variable( &v_flow_rate[t][l], 1.0 );
-
-      }
-     }
-    }
-
-    if( !v_end_arc.empty() ) {
-      if( v_end_arc[l] == n ) {
-       if( t - v_downhill_delay[ l ] >= 0 &&
-           t - v_downhill_delay[ l ] <= f_time_horizon ) {
-
-        linear_function->add_variable( &v_flow_rate[ t - v_downhill_delay[ l ] ][ l ], -1.0 );
-     } else if ( v_downhill_delay[ l ] == 0 ) {
-        linear_function->add_variable( &v_flow_rate[t][l], -1.0 );
-       }
-    }
-   }
-   }
-   FinalVolumeReservoir_Const[constraint_index][n].set_both(  v_inflows[n][constraint_index] );
-   FinalVolumeReservoir_Const[constraint_index][n].set_function( linear_function );
-  }
-  }
+ // for( Index n = 0; n < f_number_reservoirs; ++n ) {
+ //
+ //  auto l_f = new LinearFunction();
+ //
+ //  l_f->add_variable( &v_volumetric[n][0], 1.0 );
+ //
+ //  // end += n;
+ //
+ //  for( Index l = 0; l < f_number_arcs; ++l ) {
+ //
+ //   if( v_start_arc[l] == n ) {
+ //    if( -v_uphill_delay[l] >= 0 &&
+ //        -v_uphill_delay[l] <= f_time_horizon ) {
+ //     l_f->add_variable( &v_flow_rate[0 - v_uphill_delay[l]][l], 1.0 );
+ //    } else if( v_uphill_delay[l] == 0 ) {
+ //     l_f->add_variable( &v_flow_rate[0][l], 1.0 );
+ //    }
+ //   }
+ //
+ //   if( v_end_arc[l] == n ) {
+ //    if( -v_downhill_delay[l] >= 0 &&
+ //        -v_downhill_delay[l] <= f_time_horizon ) {
+ //     l_f->add_variable( &v_flow_rate[0 - v_downhill_delay[l]][l], -1.0 );
+ //    } else if( v_downhill_delay[l] == 0 ) {
+ //     l_f->add_variable( &v_flow_rate[0][l], -1.0 );
+ //    }
+ //   }
+ //  }
+ //
+ //  FinalVolumeReservoir_Const[0][n].set_both( v_initial_volumetric[n] + v_inflows[n][0] );
+ //  FinalVolumeReservoir_Const[0][n].set_function( l_f );
+ //
+ //
+ //
+ //  for( Index t = 1, constraint_index = 1; t < f_time_horizon;
+ //       ++t, ++constraint_index  ) {
+ //
+ //   auto linear_function = new LinearFunction();
+ //
+ //   linear_function->add_variable( &v_volumetric[n][t], 1.0 );
+ //   linear_function->add_variable( &v_volumetric[n][t-1], -1.0 );
+ //
+ //   for( Index l = 0; l < f_number_arcs; ++l ) {
+ //
+ //    if( !v_start_arc.empty()) {
+ //     if( v_start_arc[l] == n ) {
+ //      if( t - v_uphill_delay[l] >= 0 &&
+ //          t - v_uphill_delay[l] <= f_time_horizon ) {
+ //
+ //       linear_function->add_variable( &v_flow_rate[t - v_uphill_delay[l]][l], 1.0 );
+ //
+ //      } else if( v_uphill_delay[l] == 0 ) {
+ //
+ //       linear_function->add_variable( &v_flow_rate[t][l], 1.0 );
+ //
+ //      }
+ //     }
+ //    }
+ //
+ //    if( !v_end_arc.empty() ) {
+ //      if( v_end_arc[l] == n ) {
+ //       if( t - v_downhill_delay[ l ] >= 0 &&
+ //           t - v_downhill_delay[ l ] <= f_time_horizon ) {
+ //
+ //        linear_function->add_variable( &v_flow_rate[ t - v_downhill_delay[ l ] ][ l ], -1.0 );
+ //     } else if ( v_downhill_delay[ l ] == 0 ) {
+ //        linear_function->add_variable( &v_flow_rate[t][l], -1.0 );
+ //       }
+ //    }
+ //   }
+ //   }
+ //   FinalVolumeReservoir_Const[constraint_index][n].set_both(  v_inflows[n][constraint_index] );
+ //   FinalVolumeReservoir_Const[constraint_index][n].set_function( linear_function );
+ //  }
+ //  }
 
  add_static_constraint( FinalVolumeReservoir_Const, "FinalVolumeReservoir");
 
