611ad596237676febf36a3fef0e867390638bbc2
Ali Ghezel <ali.nov7@gmail.com>
2020-11-19 11:00:19 +0100

    Fixed IntermittentUnitBlock constraints bug

--- src/IntermittentUnitBlock.cpp
+++ src/IntermittentUnitBlock.cpp
@@ -104,9 +104,11 @@ void IntermittentUnitBlock::deserialize( netCDF::NcGroup & group ) {
 
  ::deserialize( group, "InertiaPower", v_inertia_power, true );
 
- ::deserialize( group, "Gamma", &f_gamma );
+ ::deserialize( group, "Gamma", &f_gamma, false );
 
- ::deserialize( group, "Kappa", &f_kappa );
+if (! ::deserialize( group, "Kappa", &f_kappa, true )) {
+ f_kappa = 1;
+}
 
  decompress_vector( v_minimum_power );
  decompress_vector( v_maximum_power );
@@ -177,69 +179,13 @@ void IntermittentUnitBlock::generate_abstract_constraints
  if( constraints_generated() )
   return; // constraints have already been generated
 
- // initial condition of each vector
- std::vector<double> min_power = v_minimum_power;
- if (min_power.size() == 1) {
-  min_power.resize(f_time_horizon, f_kappa * min_power[0]);
-
- } else if (min_power.size() < f_time_horizon) {
-  min_power.resize(f_time_horizon);
-  int j = 0;
-  for (unsigned long i = 0; i < v_change_intervals.size(); ++i) {
-   Index sup;
-   if (i == v_change_intervals.size() - 1 ) {
-    sup = f_time_horizon;
-   } else {
-    sup = v_change_intervals[i];
-   }
-   for (; j < sup; ++j) {
-    min_power[j] = f_kappa * v_minimum_power[i];
-   }
-  }
- }
-
  std::vector<double> max_power = v_maximum_power;
- if( max_power.size() == 1 ) {
-  max_power.resize( f_time_horizon, f_kappa * max_power[0] );
- }else if (max_power.size() < f_time_horizon) {
-  max_power.resize(f_time_horizon);
-  int j = 0;
-  for (unsigned long i = 0; i < v_change_intervals.size(); ++i) {
-   Index sup;
-   if (i == v_change_intervals.size() - 1 ) {
-    sup = f_time_horizon;
-   } else {
-    sup = v_change_intervals[i];
-   }
-   for (; j < sup; ++j) {
-    max_power[j] = f_kappa * v_maximum_power[i];
-   }
-  }
- }
+ std::vector<double> min_power = v_minimum_power;
 
 /*--------------------------------------------------------------------------*/
  // Initializing maximum power constraints
 
- if( f_gamma != 0 ) {
-  // INITIAL CONDITION
-  std::vector< double > Max_power_kappa_gamma = v_maximum_power;
-  if( Max_power_kappa_gamma.size() == 1 ) {
-   Max_power_kappa_gamma.resize( f_time_horizon, f_kappa * f_gamma * Max_power_kappa_gamma[0] );
-  } else if( Max_power_kappa_gamma.size() < f_time_horizon ) {
-   Max_power_kappa_gamma.resize( f_time_horizon );
-   int j = 0;
-   for( unsigned long i = 0; i < v_change_intervals.size(); ++i ) {
-    Index sup;
-    if( i == v_change_intervals.size() - 1 ) {
-     sup = f_time_horizon;
-    } else {
-     sup = v_change_intervals[i];
-    }
-    for( ; j < sup; ++j ) {
-     Max_power_kappa_gamma[j] = f_kappa * f_gamma * v_maximum_power[i];
-    }
-   }
-  }
+
   if( MaxPower_Constraints.size() != f_time_horizon ) {
    // this should only happen once
    assert( MaxPower_Constraints.empty());
@@ -247,17 +193,15 @@ void IntermittentUnitBlock::generate_abstract_constraints
    MaxPower_Constraints.resize( f_time_horizon );
   }
 
-
   for( Index t = 0; t < f_time_horizon; ++t ) {
 
    auto linear_function = new LinearFunction();
-
    linear_function->add_variable( &v_active_power[t], f_gamma );
    linear_function->add_variable( &v_primary_spinning_reserve[t], 1.0 );
    linear_function->add_variable( &v_secondary_spinning_reserve[t], 1.0 );
 
    MaxPower_Constraints[t].set_lhs( -Inf< double >());
-   MaxPower_Constraints[t].set_rhs(( Max_power_kappa_gamma[t] ));
+   MaxPower_Constraints[t].set_rhs(( f_gamma * f_kappa * (max_power[t]) ));
    MaxPower_Constraints[t].set_function( linear_function );
   }
 
@@ -281,13 +225,12 @@ void IntermittentUnitBlock::generate_abstract_constraints
    linear_function->add_variable( &v_primary_spinning_reserve[t], -1.0 );
    linear_function->add_variable( &v_secondary_spinning_reserve[t], -1.0 );
 
-   MinPower_Constraints[t].set_lhs( min_power[t] );
+   MinPower_Constraints[t].set_lhs( f_kappa * min_power[t] );
    MinPower_Constraints[t].set_rhs( Inf< double >());
    MinPower_Constraints[t].set_function( linear_function );
   }
 
   add_static_constraint( MinPower_Constraints, "MinPower_Intermittent" );
- }
 
  // Initializing active power bounds constraints
  if( active_power_bounds_Constraints.size() != f_time_horizon ) {
@@ -303,8 +246,8 @@ void IntermittentUnitBlock::generate_abstract_constraints
 
   linear_function->add_variable( &v_active_power[t], 1.0 );
 
-  active_power_bounds_Constraints[t].set_lhs( min_power[ t ]);
-  active_power_bounds_Constraints[t].set_rhs( max_power[ t ] );
+  active_power_bounds_Constraints[t].set_lhs( f_kappa * min_power[ t ]);
+  active_power_bounds_Constraints[t].set_rhs( f_kappa * max_power[ t ] );
   active_power_bounds_Constraints[t].set_function( linear_function );
  }
 
