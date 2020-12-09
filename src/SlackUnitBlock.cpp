9ba774c49d2b0dfd463cc0b58d5b33ba03662f9e
Ali Ghezel <ali.nov7@gmail.com>
2020-12-09 17:43:43 +0100

    fixed

--- src/SlackUnitBlock.cpp
+++ src/SlackUnitBlock.cpp
@@ -113,7 +113,7 @@ void SlackUnitBlock::generate_abstract_variables
   // Commitment Variable
   v_commitment.resize( f_time_horizon );
    for( auto & i : v_commitment )
-   // i.set_type( ColVariable::kPosUnitary );
+    i.set_type( ColVariable::kPosUnitary );
     add_static_variable( v_commitment, "u_inertia"  );
 
 
