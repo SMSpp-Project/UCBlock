f879a11cce1ca72e49075c80959e2995cc285961
Ali Ghezel <ali.nov7@gmail.com>
2020-12-15 09:07:05 +0100

    Fixing minor error in Battery

--- src/BatteryUnitBlock.cpp
+++ src/BatteryUnitBlock.cpp
@@ -103,7 +103,7 @@ void BatteryUnitBlock::deserialize( netCDF::NcGroup & group ) {
                                               "DeltaRampUp",
                                               "DeltaRampDown",
                                               "StoringBatteryRho",
-                                              "ExtractingBatterRho",
+                                              "ExtractingBatteryRho",
                                               "InitialStorage",
                                               "Cost",
                                               "Demand" };
