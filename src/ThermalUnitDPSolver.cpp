15cae90da9f145c85c85176d02b98cb45af562e7
Rafael Lobato <rafael.lobato@gmail.com>
2022-01-11 02:47:09 -0300

    Fix ThermalUnitDPSolver::get_var_solution()
    Replace the bitwise NOT by the logical not.

--- src/ThermalUnitDPSolver.cpp
+++ src/ThermalUnitDPSolver.cpp
@@ -112,7 +112,7 @@ void ThermalUnitDPSolver::get_var_solution( Configuration * solc )
 
   // startup at i iff the unit was off at i - 1 and it is on at i
   for( Index i = std:: max( t_init , Index( 1 ) ) ; i < time_horizon ; ++i )
-   (sup_it++)->set_value( U[ i ] && ( ~ U[ i - 1 ] ) ? 1 : 0 );
+   (sup_it++)->set_value( U[ i ] && ( ! U[ i - 1 ] ) ? 1 : 0 );
   }
 
  // set shut_down variables, if any, but note that start_up variables are
@@ -120,12 +120,12 @@ void ThermalUnitDPSolver::get_var_solution( Configuration * solc )
  if( auto sdn_it = b->get_shut_down() ) {
   // shutdown at 0 iif the unit was on at the start and it is off at 0
   if( ! t_init )
-   (sdn_it++)->set_value( ( init_up_down_time > 0 ) && ( ~ U[ 0 ] )
+   (sdn_it++)->set_value( ( init_up_down_time > 0 ) && ( ! U[ 0 ] )
 			  ? 1 : 0 );
 
   // shutdown at i iff the unit was on at i - 1 and it is off at i
   for( Index i = std::max( t_init , Index( 1 ) ) ; i < time_horizon ; ++i )
-   (sdn_it++)->set_value( ( ~ U[ i ] ) && U[ i - 1 ] ? 1 : 0 );
+   (sdn_it++)->set_value( ( ! U[ i ] ) && U[ i - 1 ] ? 1 : 0 );
   }
 
  // unlock the Block
