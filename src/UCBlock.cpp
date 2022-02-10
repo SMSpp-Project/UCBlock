b32bf4117441c6e5452bd477d68fa6ff4346f365
Rafael Lobato <rafael.lobato@gmail.com>
2022-02-10 00:30:52 -0300

    The netCDF variable "PollutantBudget" must be handled correctly

--- src/UCBlock.cpp
+++ src/UCBlock.cpp
@@ -254,9 +254,11 @@ void UCBlock::deserialize( const netCDF::NcGroup & group )
   ::deserialize( group , "PollutantZones" ,
                  v_pollutant_zones , true , true );
 
+  /* TODO commented away until this is properly managed
   ::deserialize( group , "PollutantBudget" ,
                  { f_total_number_pollutant_zones } ,
                  v_pollutant_budget , true , false );
+  */
 
  ::deserialize( group , "PollutantRho" ,
                 v_pollutant_rho , true , true );
@@ -1424,8 +1426,10 @@ void UCBlock::serialize( netCDF::NcGroup & group ) const {
  ::serialize( group , "PollutantZones" , netCDF::NcUint() ,
               { NumberPollutants , NumberNodes } , v_pollutant_zones );
 
+ /* TODO commented away until this is properly managed
  ::serialize( group , "PollutantBudget" , netCDF::NcDouble() ,
               TotalNumberPollutantZones , v_pollutant_budget );
+ */
 
  ::serialize( group , "PollutantRho" , netCDF::NcDouble() ,
               { TimeHorizon , NumberPollutants , NumberElectricalGenerators } ,
