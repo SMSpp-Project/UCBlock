5d328420df5f4a74d9fb6d08740817d1c4963da3
Donato Meoli <donato.meoli.95@gmail.com>
2023-05-11 21:32:53 +0200

    added doc

--- src/ThermalUnitBlock.cpp
+++ src/ThermalUnitBlock.cpp
@@ -446,7 +446,7 @@ void ThermalUnitBlock::generate_abstract_variables( Configuration * stvv )
 
  UnitBlock::generate_abstract_variables( stvv );
 
- Index wf = 1;
+ Index wf = 1;  // T formulation
  if( ( ! stvv ) && f_BlockConfig )
   stvv = f_BlockConfig->f_static_variables_Configuration;
  if( auto sci = dynamic_cast< SimpleConfiguration< int > * >( stvv ) )
