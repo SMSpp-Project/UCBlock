81a40df0efa9ce38570003e5737c2481f315b44a
Donato Meoli <donato.meoli.95@gmail.com>
2023-05-16 01:50:34 +0200

    removed node injection upper bounds in NC case

--- src/ECNetworkBlock.cpp
+++ src/ECNetworkBlock.cpp
@@ -237,6 +237,8 @@ void ECNetworkBlock::generate_abstract_constraints( Configuration * stcc )
  if( constraints_generated() )  // constraints have already been generated
   return;                       // nothing to do
 
+ const auto is_coop = is_cooperative();
+
  const auto number_nodes = get_number_nodes();
  const auto number_intervals = get_number_intervals();
 
@@ -280,7 +282,7 @@ void ECNetworkBlock::generate_abstract_constraints( Configuration * stcc )
  //    P^M <= P^-       for all u, t     (2)
  // => P^M - P^- <= 0   for all u, t
 
- if( is_cooperative() ) {
+ if( is_coop ) {
 
   power_shared_const.resize(
    boost::multi_array< FRowConstraint , 2 >::extent_gen()
@@ -375,23 +377,27 @@ void ECNetworkBlock::generate_abstract_constraints( Configuration * stcc )
 
  // node injection upper bound constraints
 
- node_injection_upper_bound_const.resize(
-  boost::multi_array< FRowConstraint , 2 >::extent_gen()
-  [ number_nodes ][ number_intervals ] );
+ if( is_coop ) {
 
- for( Index t = 0 ; t < number_intervals ; ++t )
+  node_injection_upper_bound_const.resize(
+   boost::multi_array< FRowConstraint , 2 >::extent_gen()
+   [ number_nodes ][ number_intervals ] );
 
-  for( Index node_id = 0 ; node_id < number_nodes ; ++node_id ) {
+  for( Index t = 0 ; t < number_intervals ; ++t )
 
-   node_injection_upper_bound_const[ node_id ][ t ].set_lhs( -Inf< double >() );
-   node_injection_upper_bound_const[ node_id ][ t ].set_rhs(
-    v_MaxNodeInjection[ t ][ node_id ] );
-   node_injection_upper_bound_const[ node_id ][ t ].set_variable(
-    &v_node_injection[ t ][ node_id ] );
-  }
+   for( Index node_id = 0 ; node_id < number_nodes ; ++node_id ) {
 
- add_static_constraint( node_injection_upper_bound_const ,
-                        "Node_Injection_Upper_Bound_Const_Network" );
+    node_injection_upper_bound_const[ node_id ][ t ].set_lhs(
+     -Inf< double >() );
+    node_injection_upper_bound_const[ node_id ][ t ].set_rhs(
+     v_MaxNodeInjection[ t ][ node_id ] );
+    node_injection_upper_bound_const[ node_id ][ t ].set_variable(
+     &v_node_injection[ t ][ node_id ] );
+   }
+
+  add_static_constraint( node_injection_upper_bound_const ,
+                         "Node_Injection_Upper_Bound_Const_Network" );
+ }
 
  set_constraints_generated();
 
