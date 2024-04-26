51e8fe9b56a3c581c73b7d2171800ef417553b06
Donato Meoli <donato.meoli.95@gmail.com>
2024-04-26 23:34:13 +0200

    fixed spaces

--- src/ThermalUnitDPSolver.cpp
+++ src/ThermalUnitDPSolver.cpp
@@ -704,19 +704,19 @@ bool ThermalUnitDPSolver::guts_of_process_modifications( const p_Mod mod )
      return( false );
 
     case( ThermalUnitBlockMod::eSetLinT ):
-     retrieve_term( linear_term, b->get_linear_term() );
+     retrieve_term( linear_term , b->get_linear_term() );
      if( stage > graph_OK )
       stage = graph_OK;
      return( false );
 
     case( ThermalUnitBlockMod::eSetQuadT ):
-     retrieve_term( quad_term, b->get_quad_term() );
+     retrieve_term( quad_term , b->get_quad_term() );
      if( stage > graph_OK )
       stage = graph_OK;
      return( false );
 
     case( ThermalUnitBlockMod::eSetConstT ):
-     retrieve_term( const_term, b->get_const_term() );
+     retrieve_term( const_term , b->get_const_term() );
      stage = start;
      return( false );
 
