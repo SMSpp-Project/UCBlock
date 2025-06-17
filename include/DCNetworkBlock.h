c4c6b09cde07c7447abd4984b4de31b116c1c101
U-ATLAS\d34552 <d34552@DSP0961794.atlas.edf.fr>
2025-06-17 14:59:39 +0200

    repaired problem with AC/DC line getters

--- include/DCNetworkBlock.h
+++ include/DCNetworkBlock.h
@@ -370,8 +370,8 @@ class DCNetworkData : public NetworkData
  std::vector< Index > get_AC_lines( void ) {
   std::vector< Index > AC_lines;
   const auto & susceptance = get_line_susceptance();
-  if( AC_lines.empty() )
-   return( AC_lines );
+  //if( AC_lines.empty() )
+  // return( AC_lines );
   for( Index line_id = 0 ; line_id < get_number_lines() ;
        ++line_id ) {
    if( susceptance[ line_id ] > 0. )
@@ -388,11 +388,10 @@ class DCNetworkData : public NetworkData
  std::vector< Index > get_DC_lines( void ) {
   std::vector< Index > DC_lines;
   const auto & susceptance = get_line_susceptance();
-  for( Index line_id = 0 ; line_id < get_number_lines() ;
-       ++line_id ) {
-   if( ( susceptance.empty() ) || ( susceptance[ line_id ] == 0. ) )
-    DC_lines.push_back( line_id );
-   }
+  for( Index line_id = 0 ; line_id < get_number_lines() ; ++line_id ) {
+    if( susceptance[ line_id ] == 0. )
+      DC_lines.push_back( line_id );
+  }
   return( DC_lines );
   }
 /*--------------------------------------------------------------------------*/
