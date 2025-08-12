62c831f82f49ee23fe80d79eefde98cd8d5583e1
U-ATLAS\d34552 <d34552@DSP0961794.atlas.edf.fr>
2025-08-12 12:48:39 +0200

    added linking constraint between q and p

--- include/ThermalUnitBlock.h
+++ include/ThermalUnitBlock.h
@@ -2867,6 +2867,10 @@ class ThermalUnitBlock : public UnitBlock
  /// the reactive power bound constraints
  std::vector< BoxConstraint > ReactivePower_Bound_Const;
 
+ /// Q <= P
+ std::vector< FRowConstraint > Reactive_2_Active_Const;
+
+
  /// the objective function
  FRealObjective objective;
 
