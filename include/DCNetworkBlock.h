5727f6086d88c854549c71c6209072db4a81f3bc
Antonio Frangioni <frangio@di.unipi.it>
2026-04-10 11:06:36 +0200

    added missing std::

--- include/DCNetworkBlock.h
+++ include/DCNetworkBlock.h
@@ -648,7 +648,7 @@ class DCNetworkData : public NetworkData
 
 /*- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
 
- const std::map< Index, set< Index > > & get_spanning_tree( void ) {
+ const std::map< Index, std::set< Index > > & get_spanning_tree( void ) {
   if( ! cycle_basis_was_computed )
    this->compute_cycle_basis();
   return( m_spanning_tree );
