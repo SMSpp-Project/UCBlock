b938ab3a4e99d81b248a7d38edee78018109caa7
dmeoli <donato.meoli.95@gmail.com>
2026-05-17 01:58:59 +0200

    updated doc

--- src/OTSNetworkBlock.cpp
+++ src/OTSNetworkBlock.cpp
@@ -8,7 +8,8 @@
  *         Dipartimento di Informatica \n
  *         Universita' di Pisa \n
  *
- * \author Claude 4.6 \n
+ * \author Claude Opus 4.7 \n
+ *         Anthropic
  *
  * \copyright &copy; by Antonio Frangioni
  */
